be10a07b2e7db95ede17467953ea19c3b75f742d
linocz <s617218165@gmail.com>
2023-07-03 23:59:31 +0000

    43

--- stream/client.py
+++ stream/client.py
@@ -1,17 +1,18 @@
 import grpc
+import asyncio
 import timestamp_pb2
 import timestamp_pb2_grpc
 
 
-def run():
-    with grpc.insecure_channel('localhost:50052') as channel:
+async def run():
+    async with grpc.aio.insecure_channel('localhost:50098') as channel:
         stub = timestamp_pb2_grpc.TimestampServiceStub(channel)
         try:
-            for timestamp in stub.GetTimestampStream(timestamp_pb2.Empty()):
+            async for timestamp in stub.GetTimestampStream(timestamp_pb2.Empty()):
                 print(f"Received message: {timestamp.message}, Time: {timestamp.timestamp}")
         except KeyboardInterrupt:
             print("Keyboard interrupt received. Stopping program...")
 
 
 if __name__ == '__main__':
-    run()
+    asyncio.run(run())
--- stream/server.py
+++ stream/server.py
@@ -2,6 +2,7 @@ import pandas as pd
 import contextvars
 import time
 import grpc
+import asyncio
 
 import timestamp_pb2
 import timestamp_pb2_grpc
@@ -11,29 +12,30 @@ from concurrent import futures
 
 class TimestampService(timestamp_pb2_grpc.TimestampServiceServicer):
     def __init__(self):
-        self.df = contextvars.ContextVar('df', default=pd.DataFrame([]))
+        pass
 
-    def GetTimestampStream(self, request_iterator, context):
+    async def GetTimestampStream(self, request_iterator, context):
         while True:
             current_time = time.time()
-            print(f"Current time: {current_time}, df: {self.df.get()}")
+            print(f"Current time: {current_time}")
 
             yield timestamp_pb2.Timestamp(
                 message=f"Current time: {current_time}",
                 timestamp=int(current_time),
             )
-            time.sleep(1)
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+            await asyncio.sleep(1)
+
+async def serve():
+    server = grpc.aio.server()
     timestamp_pb2_grpc.add_TimestampServiceServicer_to_server(TimestampService(), server)
-    server.add_insecure_port("[::]:50052")
-    server.start()
+    server.add_insecure_port("[::]:50098")
+    await server.start()
 
     try:
-        server.wait_for_termination()
+        await server.wait_for_termination()
     except KeyboardInterrupt:
         server.stop(0)
 
 if __name__ == "__main__":
-    serve()
+    asyncio.run(serve())
